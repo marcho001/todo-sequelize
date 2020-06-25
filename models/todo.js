9dc07b01927181ba8250dc211310b2390af76022
Marc <g40419@gmail.com>
2020-06-25 16:12:32 +0800

    feat: create associate between todo & user

--- models/todo.js
+++ models/todo.js
@@ -11,6 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Todo.belongsTo(models.User)
+
     }
   };
   Todo.init({
