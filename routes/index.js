303c71b226f896d1b44348bcc5d49ad0c07f3818
Marc <g40419@gmail.com>
2020-06-26 20:43:16 +0800

    feat: add authenticator middleware 檢查是否是登入狀態 登入才能使用功能

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,11 @@ const router = express.Router()
 const user = require('./modules/user')
 const home = require('./modules/home')
 const todo = require('./modules/todo')
+const auth = require('../middleware/auth').authenticator
 
 router.use('/users', user)
-router.use('/', home)
-router.use('/todos', todo)
+router.use('/todos', auth, todo)
+router.use('/', auth, home)
 
 
 
