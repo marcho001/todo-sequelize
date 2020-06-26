d8e58d05ecacb8a78b83720771b6a3fc1f487995
Marc <g40419@gmail.com>
2020-06-26 21:40:10 +0800

    feat: 新增 刪除todo router

--- routes/modules/todo.js
+++ routes/modules/todo.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
+router.get('/new', (req, res) => res.render('new'))
+
 router.get('/:id', (req, res) => {
   const id = req.params.id
   return Todo.findByPk(id)
@@ -14,21 +16,30 @@ router.get('/:id', (req, res) => {
 router.get('/:id/edit', (req, res) => {
   const id = req.params.id
   return Todo.findByPk(id)
-    .then(todo => res.render('edit', { todo }))
+    .then(todo => res.render('edit', { todo: todo.toJSON() }))
+    .catch(error => console.log('edit', error))
 })
 
 router.put('/:id', (req, res) => {
-  console.log('put')
   const id = req.params.id
   const { name, isDone } = req.body
   return Todo.findByPk(id)
     .then(todo => {
+      console.log(todo)
       todo.name = name,
       todo.isDone = isDone === 'on'
       return todo.save()
     })
     .then(() => res.redirect(`/todos/${id}`))
     .catch(error => console.log('put err',error))
+})
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => todo.destroy())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 module.exports = router
