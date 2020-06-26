2374e72fcafd931efb5ec488974449c789b3289d
Marc <g40419@gmail.com>
2020-06-26 22:37:09 +0800

    refactor: all router 加入UserId條件 達成只能操作該user's todos

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,9 +4,11 @@ const db = require('../../models')
 const Todo = db.Todo
 
 router.get('/', (req, res) => {
+  const UserId = req.user.id
   return Todo.findAll({
     raw: true,
-    nest: true
+    nest: true,
+    where: { UserId }
   })
     .then(todos => res.render('index', { todos }))
     .catch(error => res.status(422).json(error))
--- routes/modules/todo.js
+++ routes/modules/todo.js
@@ -7,23 +7,26 @@ router.get('/new', (req, res) => res.render('new'))
 
 router.get('/:id', (req, res) => {
   const id = req.params.id
-  return Todo.findByPk(id)
+  const UserId = req.user.id
+  return Todo.findOne({ where:{ id, UserId }})
     .then(todo => res.render('detail', { todo: todo.toJSON() })
     )
     .catch(error => console.log(error))
 })
 
 router.get('/:id/edit', (req, res) => {
+  const UserId = req.user.id
   const id = req.params.id
-  return Todo.findByPk(id)
+  return Todo.findOne({ where:{ id, UserId }})
     .then(todo => res.render('edit', { todo: todo.toJSON() }))
     .catch(error => console.log('edit', error))
 })
 
 router.put('/:id', (req, res) => {
+  const UserId = req.user.id
   const id = req.params.id
   const { name, isDone } = req.body
-  return Todo.findByPk(id)
+  return Todo.findOne({ where:{ id, UserId }})
     .then(todo => {
       todo.name = name,
       todo.isDone = isDone === 'on'
@@ -34,9 +37,9 @@ router.put('/:id', (req, res) => {
 })
 
 router.delete('/:id', (req, res) => {
-  console.log(req.user)
+  const UserId = req.user.id
   const id = req.params.id
-  return Todo.findByPk(id)
+  return Todo.findOne({ where:{ id, UserId }})
     .then(todo => todo.destroy())
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
